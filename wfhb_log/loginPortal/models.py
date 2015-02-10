687163ff4dfbc8fc43263e9c760d8c647a445edd
jds <jdsprink@umail.iu.edu>
2015-02-10 11:58:26 -0500

    added comment

--- wfhb_log/loginPortal/models.py
+++ wfhb_log/loginPortal/models.py
@@ -1,6 +1,8 @@
-from django.db import models
+ from django.db import models
 
 # Create your models here.
+
+# don't tell me what to do
 
 class Person(models.Model):
 	email = models.EmailField(max_length=75, primary_key=True)
