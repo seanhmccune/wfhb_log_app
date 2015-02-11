4b3a2700fda7d0a875a15f73a2288902ad011d50
jds <jdsprink@umail.iu.edu>
2015-02-11 16:07:10 -0500

    some changes

--- wfhb_log/loginPortal/models.py
+++ wfhb_log/loginPortal/models.py
@@ -8,31 +8,55 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 # don't tell me what to do
 
 # this allows us to create a new volunteer 
+
+class Contact(model.models):
+	contact_first_name = models.CharField(max_length=25)
+	contact_last_name = models.CharField(max_length=25)
+	contact_phone_number = models.CharField(max_length=100)
+	relation_to_contact = models.CharField(max_length=200)
+
 class VolunteerManager(BaseUserManager):
 	
-	def create_user(self, email, password, fn, ln, address, pn, cfn, cln, cpn, rtc, dob ):
+	# this is the function that creates the a new user
+	def _create_user(email, password=None, fn, ln, address, pn, cfn, cln, cpn, rtc, dob, is_staff, is_superuser):
 		now  = timezone.now()
+		
+		# create a new contact
+		new_contact = Contact(cfn, cln, cpn, rtc)
+		
+		# check if they passed an email
 		if not email:
 			raise ValueError('The given email is not good')
 		email = self.normalize_email(email)
+		
+		# create the user
 		user = self.model(
 			email = email,
 			first_name = fn,
 			last_name = ln,
 			address = address,
 			phone_number = pn,
-			contact_first_name = cfn,
-			contact_last_name = cln,
-			contact_phone_number = cpn,
-			relation_to_contact = rtc,
 			date_of_birth = dob,
 			start_date = now,
-			is_active = False
+			e_contact = new_contact,
+			is_active = False,
+			is_staff = is_staff,
+			is_superuser = is_superuser
 		)
+		
+		# save the contact and the user 
+		new_contact.save(using=self._db)
 		user.set_password(password)
-		user.save()
+		user.save(using=self._db)
 		return user
 
+	# creates a normal user, a 'volunteer' if I may
+	def create_user(self, email, password=None, fn, ln, address, pn, cfn, cln, cpn, rtc, dob):
+		return self._create_user(email, password, fn, ln, address, pn, cfn, cln, cpn, rtc, dob, False, False)
+	
+	# creates a super user, someone who is an admin
+	def create_super_user(self, email, password, fn, ln, address, pn, cfn, cln, cpn, rtc, dob):
+		return self._create_user(email, password, fn, ln, address, pn, cfn, cln, cpn, rtc, dob, True, True)
 
 
 # this allows use to define a new user based on the volunteer class 
@@ -42,22 +66,19 @@ class Volunteer(AbstractBaseUser, PermissionsMixin):
 	# there is an implicit primary key that is declared - its just an integer 
 	# so if you look at this table there will be a primary key called 'id' that 
 	# will be an integer
-	email = models.EmailField(max_length=75, primary_key=True, unique=True)
+	email = models.EmailField(max_length=75, primary_key=True)
 	first_name = models.CharField(max_length=25)
 	last_name = models.CharField(max_length=25)
 	address = models.CharField(max_length=200)
 	phone_number = models.CharField(max_length=100)
-	contact_first_name = models.CharField(max_length=25)
-	contact_last_name = models.CharField(max_length=25)
-	contact_phone_number = models.CharField(max_length=100)
-	relation_to_contact = models.CharField(max_length=200)
 	date_of_birth = models.DateField('Birthday')
 	start_date = models.DateField('start date')
+	e_contact = models.ForeignKey(Contact, relation="Emergency Contact")
 	is_active = models.BooleanField(default=False)
 	
 	# make sure that the username field points to the email address
 	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['first_name', 'last_name', 'address', 'phone_number', 'contact_first_name', 'contact_last_name', 'contact_phone_number', 'relation_to_contact', 'date_of_birth']
+	REQUIRED_FIELDS = ['first_name', 'last_name', 'address', 'phone_number', 'date_of_birth']
 	
 	objects = VolunteerManager()
 	
