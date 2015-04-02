7934b9058cee6ed6d73195bc2c6fb6d616422dc3
jds <jdsprink@umail.iu.edu>
2015-04-01 21:13:09 -0400

    alot of stuff works right now

--- wfhb_log/wfhb_log/settings.py
+++ wfhb_log/wfhb_log/settings.py
@@ -27,7 +27,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-import os
 if (os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine') or
 	os.getenv('SETTINGS_MODE') == 'prod'):
 	# Running on production App Engine, so use a Google Cloud SQL database.
@@ -92,7 +91,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = BASE_DIR + '..' + os.sep + 'static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
